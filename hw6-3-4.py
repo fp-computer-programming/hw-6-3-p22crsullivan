de4ba65bbfb2485be144d44f70a3785e035159dc
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-11-15 18:59:21 -0500

    Submit HW 6-3

--- hw6-3-4.py
+++ hw6-3-4.py
@@ -1,1 +1,12 @@
 # Author: CRS 11/15/21
+lst = list(input("Please enter a list of numbers or letters separated by spaces."))
+answer = input("Would you like the middle or end of the input?")
+if answer == "end":
+    lst.reverse()
+    print(lst[0])
+elif answer == "middle":
+    lst.reverse()
+    lst.remove(lst[0])
+    lst.reverse()
+    lst.remove(lst[0])
+    print(lst)
